40955b339be548abbba55a7fb55101164b5c32f1
Paciente8159 <Paciente8159@users.noreply.github.com>
2025-07-19 10:54:44 +0100

    Update laser_ppi.js

--- tools/laser_ppi.js
+++ tools/laser_ppi.js
@@ -9,6 +9,8 @@ window.LaserPPIComponent = {
 	},
 	template: `<div v-if="toolMatch && this.$root.app_state.ENABLE_LASER_PPI">
 				<pin name="LASER_PPI" label="Select the Spindle Servo pin" filter="item.type.includes('generic_output') || item.type.includes('pwm')"></pin>
+				<toggle name="INVERT_LASER_PPI_LOGIC" label="Invert PPI laser signal logic" configfile="hal"
+								tooltip="Inverts the output signal logic for the PPI control pin"></toggle>
 <pin name="LASER_PPI_AIR_ASSIST" label="Select the Spindle coolant flood pin" filter="item.type.includes('generic_output')" if="app_state.ENABLE_COOLANT"></pin>
 			<pin name="SPINDLE_BESC_COOLANT_MIST" label="Select the Spindle coolant mist pin" filter="item.type.includes('generic_output')" if="app_state.ENABLE_COOLANT"></pin>
 			</div>
