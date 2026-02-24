67f5066aa9b70f667145f80b1c9ec84c946c96ff
Paciente8159 <Paciente8159@users.noreply.github.com>
2026-02-24 15:44:43 +0000

    updated tools encoders

--- tools/spindle_relay.js
+++ tools/spindle_relay.js
@@ -12,6 +12,18 @@ window.SpindleRelayComponent = {
 			<pin name="SPINDLE_RELAY_REV" label="Select the Spindle REV relay pin" filter="item.type.includes('generic_output')" configfile="hal"></pin>
 			<pin name="SPINDLE_BESC_COOLANT_FLOOD" label="Select the Spindle coolant flood pin" filter="item.type.includes('generic_output')" if="app_state.ENABLE_COOLANT" configfile="hal"></pin>
 			<pin name="SPINDLE_BESC_COOLANT_MIST" label="Select the Spindle coolant mist pin" filter="item.type.includes('generic_output')" if="app_state.ENABLE_COOLANT" configfile="hal"></pin>
+			<combobox name="SPINDLE_RELAY_RPM_ENCODER" label="Use an encoder to read the tool RPM"
+									:opts="[
+			{ id: 'ENC0', enccount: 0 },
+			{ id: 'ENC1', enccount: 1 },
+			{ id: 'ENC2', enccount: 2 },
+			{ id: 'ENC3', enccount: 3 },
+			{ id: 'ENC4', enccount: 4 },
+			{ id: 'ENC5', enccount: 5 },
+			{ id: 'ENC6', enccount: 6 },
+			{ id: 'ENC7', enccount: 7 }
+		]" valname="id" configfile="hal" nullable
+									tooltip="Assign an encoder to the tool RPM counter."></combobox>
 			</div>`
 };
 
